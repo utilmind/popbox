1581da319b73a53b21357479ebc39c7eb76352ac
Aleksey Kuznetsov <utilmind@gmail.com>
2019-04-13 23:58:51 +0300

    Add files via upload

--- popbox.js
+++ popbox.js
@@ -6,11 +6,12 @@
   var PopBox = {
 
     // public config
-    auto_show: 15000,          // in milliseconds. 15000 milliseconds = 15 seconds. 0 = disabled.
+    auto_show: 15000,         // in milliseconds. 15000 milliseconds = 15 seconds. 0 = disabled.
     auto_close: 60000,        // in milliseconds. 60000 = 60 seconds. 0 = disabled.
     show_on_scroll_start: 48, // starting scroll position in percents, between 0% and 100%. Both 0 = disabled.
     show_on_scroll_end: 52,   // ending scroll position. Eg 40..60 means that popbox will appear when any part of page between 40% and 60% is appeared in the viewport.
     closeable_on_dimmer: true,
+    auto_start_disabled: false, // disable auto_show on start. It can be re-enabled by calling PopBox.enable_auto() method.
 
     classes: {
       popbox: "popbox",
@@ -23,9 +24,13 @@
       close_countdown_digits: "close_countdown",
     },
 
+    auto_shown: false, // just helper to determinate whether popup has been already displayed. Altertatively check out "auto_disabled".
+
     // private
-    auto_shown: false,
+    auto_timer: false,
     auto_close_timer: false,
+    auto_disabled: -1, // unknown
+    scroll_hook: false,
     scrollY: 0,
     topProperties: "",
 
@@ -39,20 +44,10 @@
       doInit(function() {
         if (typeof $=="undefined") return 1;
 
-        // auto-start popup in N seconds
+        // click on workarea
         $("."+me.classes.popbox+" ."+me.classes.workarea).click(function(e) {
           e.stopPropagation(); // prevent "close on dimmer" event when the click inside the form
           // stop_hide_timer(); // no, let's stop timer only when user starts filling the form.
-        });
-
-        // stop auto-hide timer when user started type something...
-        $("."+me.classes.popbox+" input").keydown(function(e) {
-          me.stop_hide_timer();
-        });
-
-        // close on X-button
-        $("."+me.classes.close_button).click(function(e) {
-          me.hide();
         });
 
         // close on dimmer
@@ -62,64 +57,98 @@
           });
         }
 
-        // hook scroll
-        if (me.show_on_scroll_end > me.show_on_scroll_start) {
-          $(window).scroll(function(e) {
-            var scrollTop = $(window).scrollTop(),
-                viewport_height = $(window).height();
-
-            // This is a kludge to make it working in quirks mode. Problem described at https://viralpatel.net/blogs/jquery-window-height-incorrect/
-            // In case if page doesn't have <!DOCTYPE HTML> or similar directive, the document.body.clientHeight is the viewport height, and $(window).height() is the whole page height.
-            // I would prefer to make this script compatible in any mode, so let's just check out both and use lesser value.
-            if (viewport_height > document.body.clientHeight)
-              viewport_height = document.body.clientHeight;
-
-            // console.log("top " + scrollTop + ", bottom: " + (scrollTop + document.body.clientHeight) + ', view height: ' + document.body.clientHeight + ' / ' + $(window).height() + ' viewport: ' + viewport_height +
-            //  ", start: " + document.body.scrollHeight / 100 * me.show_on_scroll_start + ", end: " + document.body.scrollHeight / 100 * me.show_on_scroll_end);
-
-            if (((scrollTop + viewport_height) > (document.body.scrollHeight / 100 * me.show_on_scroll_start)) &&
-                (scrollTop < (document.body.scrollHeight / 100 * me.show_on_scroll_end))) {
-              // console.log('fire')
-              me.show(me.auto_close);
-            }
-          });
-        }
+        // close on X-button
+        $("."+me.classes.close_button).click(function(e) {
+          me.hide();
+        });
 
         // ESC key hook
         $(document).keydown(function(e) {
-          if ($("."+me.classes.popbox).is(":visible") && (e.keyCode == 27)) {
+          if ((e.keyCode == 27) && me.is_visible()) {
             e.preventDefault();
             e.stopPropagation();
             me.hide();
           }
         });
 
-        // start auto-show timer after the page will be fully loaded.
-        doInit(function() {
-          if (me.auto_show) {
-            setTimeout(function() {
-              me.show(me.auto_close);
-            }, me.auto_show);
-          }
-        }, 2); // 2 - after full page load
+        // stop auto-hide timer when user started type something...
+        $("."+me.classes.popbox+" input").keydown(function(e) {
+          me.stop_hide_timer();
+        });
+
+        if (!me.auto_start_disabled) {
+          doInit(function() {
+            me.enable_auto();
+          }, 2); // 2 - after full page load
+        }
       });
+    },
+
+    enable_auto: function() {
+      var me = this;
+      if (!me.auto_disabled) return; // nothing to do
+
+      me.auto_disabled = false;
+
+      // start auto-show timer after the page will be fully loaded.
+      if (me.auto_show) {
+        me.auto_timer = setTimeout(function() {
+          me.show(me.auto_close, 1);
+        }, me.auto_show);
+      }
+
+      // hook scroll
+      if (me.show_on_scroll_end > me.show_on_scroll_start) {
+        $(window).scroll(me.scroll_hook = function(e) {
+          var scrollTop = $(window).scrollTop(),
+              viewport_height = $(window).height();
+
+          // This is a kludge to make it working in quirks mode. Problem described at https://viralpatel.net/blogs/jquery-window-height-incorrect/
+          // In case if page doesn't have <!DOCTYPE HTML> or similar directive, the document.body.clientHeight is the viewport height, and $(window).height() is the whole page height.
+          // I would prefer to make this script compatible in any mode, so let's just check out both and use lesser value.
+          if (viewport_height > document.body.clientHeight)
+            viewport_height = document.body.clientHeight;
+
+          // console.log("top " + scrollTop + ", bottom: " + (scrollTop + document.body.clientHeight) + ', view height: ' + document.body.clientHeight + ' / ' + $(window).height() + ' viewport: ' + viewport_height +
+          //  ", start: " + document.body.scrollHeight / 100 * me.show_on_scroll_start + ", end: " + document.body.scrollHeight / 100 * me.show_on_scroll_end);
+
+          if (((scrollTop + viewport_height) > (document.body.scrollHeight / 100 * me.show_on_scroll_start)) &&
+              (scrollTop < (document.body.scrollHeight / 100 * me.show_on_scroll_end))) {
+            // console.log('fire')
+            me.show(me.auto_close, 1);
+          }
+        });
+      }
+    },
+
+    disable_auto: function() {
+      var me = this;
+      if (me.auto_disabled > 0) return; // nothing to do
+
+      me.auto_disabled = true;
+
+      if (me.auto_timer)
+        clearInterval(me.auto_timer);
+      if (me.scroll_hook)
+        $(window).unbind("scroll");
     },
 
     is_visible: function() {
       return $("."+this.classes.popbox).is(":visible");
     },
 
-    show: function(auto_close_ms) {
+    show: function(auto_close_ms, is_auto) {
       var me = this;
-      if (me.is_visible() || me.auto_shown) return;
+      if (me.is_visible()) return;
 
-      me.override_scrollbar(1);
-      $("."+me.classes.popbox).show();
+      if (is_auto) {
+        if (me.auto_disabled) return;
+        me.disable_auto();
+        me.auto_shown = 1; // just a flag that we already displayed it.
 
-      if (auto_close_ms) {
-        me.auto_shown = 1;
+        // countdowns and misc stuff
         $("."+me.classes.close_countdown).show();
-
+    
         var auto_close_ticks = auto_close_ms / 1000;
         $("."+me.classes.close_countdown_digits).html(auto_close_ticks);
 
@@ -134,8 +163,8 @@
       }else
         $("."+me.classes.close_countdown).hide();
 
-//      if (me.show_on_scroll_end > me.show_on_scroll_start)
-//        $(window).unbind("scroll");
+      me.override_scrollbar(1);
+      $("."+me.classes.popbox).show();
     },
 
     hide: function() {
