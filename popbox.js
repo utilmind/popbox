aa3737d90b8d8cdd101718f4325d59a17469e295
Aleksey Kuznetsov <utilmind@gmail.com>
2019-10-22 15:24:42 -0400

    Add files via upload

--- popbox.js
+++ popbox.js
@@ -1,4 +1,5 @@
 // PopBox. Written by AK 12.IV.2019 for "Palm Beach Cuisine". Copyright (c) 2019, UtilMind Solutions.
+// This code requires doInit() from https://github.com/utilmind/on-ready-js
 
 (function(window) {
   "use strict";
@@ -6,39 +7,64 @@
   var PopBox = {
 
     // public config
-    auto_show: 15000,         // in milliseconds. 15000 milliseconds = 15 seconds. 0 = disabled.
-    auto_close: 60000,        // in milliseconds. 60000 = 60 seconds. 0 = disabled.
-    show_on_scroll_start: 48, // starting scroll position in percents, between 0% and 100%. Both 0 = disabled.
-    show_on_scroll_end: 52,   // ending scroll position. Eg 40..60 means that popbox will appear when any part of page between 40% and 60% is appeared in the viewport.
-    closeable_on_dimmer: true,
-    auto_start_disabled: false, // disable auto_show on start. It can be re-enabled by calling PopBox.enable_auto() method.
+    autoShow: 15000,         // timeout in milliseconds. 15000 milliseconds = 15 seconds. 0 = disabled.
+    autoClose: 60000,        // timeout in milliseconds. 60000 = 60 seconds. 0 = disabled.
+    autoShowDisabled: false, // disable autoShow on start. It can be re-enabled by calling PopBox.startAutoShow() method.
                                 // Alternative values:
-                                //   - "scroll": enables auto_show only after any scroll event. Don't displays popup if there was no scrolling.
-    // Callbacks. AK 26.08.2019: I thought to CustomEvent, but it's doesn't supported by IE. Don said that IE is important for PBC users. So let's use simple callbacks.
-    on_show: false, // after show
-    on_hide: false, // after hide
+                                //   - "scroll": enables autoShow only after any scroll event. Don't displays popup if there was no scrolling.
+    showOnScrollStart: 48, // starting scroll position in percents, between 0% and 100%. Both 0 = disabled.
+    showOnScrollEnd: 52,   // ending scroll position. Eg 40..60 means that popbox will appear when any part of page between 40% and 60% is appeared in the viewport.
+    closeOnDimmer: true,
+    closeOnEsc: true,
+    noPropagateClicks: false, // pass clicks on the workarea to another event handlers.
+
+    // Callbacks. AK 26.08.2019: I thought about CustomEvent, but it's doesn't supported by IE.
+    //            Donald said that IE is important for PBC users. So let's use simple callbacks. (Don't complex due to outdated methods, it's OK here.)
+    onShow: false, // after show
+    onHide: false, // after hide
 
     classes: {
       popbox: "popbox",
       workarea: "workarea",
-      fixed: "popbox-fixed",
-      no_overflow: "popbox-no-overflow",
 
-      close_button: "close_button",
-      close_countdown_text: "close_msg",
-      close_countdown_digits: "close_countdown",
+      fixed: "popbox-fixed",
+      noOverflow: "popbox-no-overflow",
+
+      closeButton: "popbox-close",
+      closeCountdownMsg: "popbox-close-msg",
+      closeCountdownDigits: "popbox-close-countdown",
     },
 
-    auto_shown: false, // just helper to determinate whether popup has been already displayed. Altertatively check out "auto_disabled".
-
     // private
-    auto_timer: false,
-    auto_close_timer: false,
-    auto_disabled: -1, // unknown
-    scroll_hook: false,
-    scrollY: 0,
-    topProperties: "",
+    timerShow: false,
+    timerClose: false,
+    scrollHook: false,
+    autoShowUsed: false,
+    showCnt: 0,
 
+    scrollPosition: {
+      y: 0,
+      top: "",
+      class: "",
+
+      save: function(bodyClassName) { // save scrollbar position
+        this.y = $(window).scrollTop(); // (document.documentElement && document.documentElement.scrollTop) || document.body.scrollTop;
+        this.top = window.getComputedStyle(document.body).top; // save properties
+        document.body.style.top = -this.y + "px";
+
+        $("body").addClass(this.class = bodyClassName);
+      },
+
+      restore: function(bodyClassName) { // restore scrollbar position
+        $("body").removeClass(bodyClassName ? bodyClassName : this.class);
+
+        document.body.style.top = this.top;
+        window.scrollTo(0, this.y);
+      },
+    },
+
+
+    // Public functions
     init: function(options) {
       var me = this;
 
@@ -47,169 +73,176 @@
         $.extend(this, options);
 
       doInit(function() {
-        if (typeof $=="undefined") return 1;
+        if (typeof $=="undefined") return 1; // wait for jQuery, then go forth.
 
         // click on workarea
-        $("."+me.classes.popbox+" ."+me.classes.workarea).click(function(e) {
-          e.stopPropagation(); // prevent "close on dimmer" event when the click inside the form
-          // stop_hide_timer(); // no, let's stop timer only when user starts filling the form.
-        });
+        if (me.noPropagateClicks)
+          $("."+me.classes.popbox+" ."+me.classes.workarea).click(function(e) {
+            e.stopPropagation(); // prevent "close on dimmer" event when the click inside the form
+            // stopTimerHide(); // no, let's stop timer only when user starts filling the form.
+          });
 
-        // close on dimmer
-        if (me.closeable_on_dimmer) {
+        // close on dimmer click
+        if (me.closeOnDimmer)
           $("."+me.classes.popbox).click(function(e) {
             e.stopPropagation();
             me.hide();
           });
-        }
 
         // close on X-button
-        $("."+me.classes.close_button).click(function(e) {
+        $("."+me.classes.closeButton).click(function(e) {
           e.stopPropagation();
+          e.preventDefault();
           me.hide();
         });
 
-        // ESC key hook
-        $(document).keydown(function(e) {
-          if ((e.keyCode == 27) && me.is_visible()) {
-            e.stopPropagation();
-            me.hide();
-          }
-        });
-
         // stop auto-hide timer when user started type something...
-        $("."+me.classes.popbox+" input").keydown(function(e) {
-          me.stop_hide_timer();
-        });
+        if (me.autoClose > 0)
+          $("."+me.classes.popbox).find("input, textarea, select").keydown(function(e) {
+            me.stopTimerHide();
+            $(this).off("keydown");
+          });
 
-        if (!me.auto_start_disabled || (me.auto_start_disabled == "scroll")) {
+        if (!me.autoShowDisabled || (me.autoShowDisabled == "scroll")) { // if false (!disabled) or "scroll"
           doInit(function() {
-            me.enable_auto();
+            me.startAutoShow(me.autoShowDisabled == "scroll"); // will be enabled either on scroll OR
           }, 2); // 2 - after full page load
         }
       });
     },
 
-    setup_auto_timer: function() { // private, for internal use
-      var me = this;
-      me.auto_timer = setTimeout(function() {
-        me.show(me.auto_close, 1);
-      }, me.auto_show);
+    isVisible: function() {
+      return $("."+this.classes.popbox).is(":visible");
     },
 
-    enable_auto: function() { // executes only after the page will be fully loaded.
+    show: function(autoCloseMs) {
       var me = this;
-      if (!me.auto_disabled) return; // nothing to do
+      if (me.isVisible()) return; // already visible right now
 
-      me.auto_disabled = false;
+      if (me.showCnt == 0)
+        me.disableAutoShow();
+      ++me.showCnt;
 
-      if (me.auto_show && (me.auto_start_disabled != "scroll"))
-        me.setup_auto_timer();
+      if (autoCloseMs) {
+        $("."+this.classes.closeCountdownMsg).show(); // popup can be displayed "manually" more than once. So if this countdown hidden, let's display it again.
 
-      // hook scroll
-      if ((me.auto_start_disabled == "scroll") || (me.show_on_scroll_end > me.show_on_scroll_start)) {
-        $(window).scroll(me.scroll_hook = function(e) {
-          if (me.auto_show && !me.auto_timer) // we needtimer, but it's not set up yet (because we're waiting for any scroll event)
-            me.setup_auto_timer();
+        // countdowns and misc stuff
+        $("."+me.classes.closeCountdownDigits).show();
 
-          var scrollTop = (document.documentElement && document.documentElement.scrollTop) || document.body.scrollTop, // $(window).scrollTop(),
-              scrollHeight = (document.documentElement && document.documentElement.scrollHeight) || document.body.scrollHeight,
+        var autoCloseTicks = autoCloseMs / 1000; // let's round to 1 second. We don't care if for lesser intervals.
+        $("."+me.classes.closeCountdownDigits).html(autoCloseTicks);
 
-              // following, window.innerHeight is better than $(window).height(), because innerHeight works both in normal and quirks mode (when <!DOCTYPE> not specified).
-              // Important! documentElement.clientHeight and body.clientHeight returns opposite values when <!DOCTYPE> specifed and not specified. Less value would be more correct.
-              viewportHeight = window.innerHeight || (document.documentElement && document.documentElement.clientHeight) || document.body.clientHeight; // window.innerHeight is undefined in IE8-.
+        me.timerClose = setInterval(function() {
 
-          if (((scrollTop + viewportHeight) > (scrollHeight / 100 * me.show_on_scroll_start)) &&
-              (scrollTop < (scrollHeight / 100 * me.show_on_scroll_end))) {
-            // console.log('popup on scroll')
-            me.show(me.auto_close, 1);
+          --autoCloseTicks;
+          $("."+me.classes.closeCountdownDigits).html(autoCloseTicks);
+
+          if (autoCloseTicks <= 0) me.hide();
+
+        }, 1000);
+      }else
+        $("."+me.classes.closeCountdownMsg).hide(); // TODO: we should hide entire message! not only digits!!
+
+      // save scroll position and show the PopBox
+      me.scrollPosition.save(me.classes.fixed + " " + me.classes.noOverflow);
+      $("."+me.classes.popbox).show();
+
+      if (me.closeOnEsc) // Hook global ESC key, but only when the popup is displayed.
+        $(document).keydown(function(e) {
+          if ((e.keyCode == 27) && me.isVisible()) {
+            e.stopPropagation();
+            me.hide();
+            $(document).off("keydown");
+          }
+        });
+
+      if (me.onShow) me.onShow();
+    },
+
+    hide: function() {
+      var me = this;
+
+      me.stopTimerHide();
+
+      // hide the PopBox and restore scroll position
+      $("."+me.classes.popbox).hide();
+      me.scrollPosition.restore();
+
+      if (me.onHide) me.onHide();
+    },
+
+
+    stopTimerHide: function() {
+      if (this.timerClose) {
+        clearInterval(this.timerClose);
+        this.timerClose = false;
+
+        $("."+this.classes.closeCountdownMsg).hide();
+      }
+    },
+
+    startAutoShow: function(isScroll) { // executes only after the page will be fully loaded. Can be invoken only once.
+      var me = this,
+
+          showPopBoxAuto = function() {
+            // The most important here: do NOT show the PopBox if it was ever displayed before.
+            if (me.showCnt == 0)
+              me.show(me.autoClose);
+            else
+              me.disableAutoShow(); // it should be already disabled, but just in case...
+          },
+
+          setupTimerShow = function() { // private, for internal use
+            if (me.autoShow && !me.timerShow) { // we need timer, but it's not set up yet (because we're waiting for any scroll event)
+
+              me.timerShow = setTimeout(function() { // save timerShow to be able to stop it.
+                showPopBoxAuto();
+              }, me.autoShow);
+
+            }
+          };
+
+      if (me.autoShowUsed) return; // already used. Don't use it twice.
+
+      me.autoShowUsed = 1;
+
+      if (!isScroll) // don't set it right now. It will be set up on first scroll movement.
+        setupTimerShow();
+
+      // hook scroll. To display the PopBox on right scroll position.
+      if (isScroll || (me.showOnScrollEnd > me.showOnScrollStart)) {
+        $(window).scroll(me.scrollHook = function(e) {
+          if (isScroll)
+            setupTimerShow(); // start the display counter after first scroll event
+
+          if (me.showOnScrollEnd > me.showOnScrollStart) {
+            var scrollTop = (document.documentElement && document.documentElement.scrollTop) || document.body.scrollTop, // $(window).scrollTop(),
+                scrollHeight = (document.documentElement && document.documentElement.scrollHeight) || document.body.scrollHeight,
+
+                // following, window.innerHeight is better than $(window).height(), because innerHeight works both in normal and quirks mode (when <!DOCTYPE> not specified).
+                // Important! documentElement.clientHeight and body.clientHeight returns opposite values when <!DOCTYPE> specifed and not specified. Less value would be more correct.
+                viewportHeight = window.innerHeight || (document.documentElement && document.documentElement.clientHeight) || document.body.clientHeight; // window.innerHeight is undefined in IE8-.
+
+            if (((scrollTop + viewportHeight) > (scrollHeight / 100 * me.showOnScrollStart)) &&
+                (scrollTop < (scrollHeight / 100 * me.showOnScrollEnd))) {
+              // console.log('popup on scroll')
+              showPopBoxAuto();
+            }
           }
         });
       }
     },
 
-    disable_auto: function() {
+    disableAutoShow: function() { // invoked when PopBox already displayed, so we should stop all counters which may invoke it.
       var me = this;
-      if (me.auto_disabled > 0) return; // nothing to do
+      if (!me.autoShowUsed) return; // there is no hooks
 
-      me.auto_disabled = true;
-
-      if (me.auto_timer)
-        clearInterval(me.auto_timer);
-      if (me.scroll_hook)
+      if (me.timerShow)
+        clearInterval(me.timerShow);
+      if (me.scrollHook)
         $(window).unbind("scroll");
     },
-
-    is_visible: function() {
-      return $("."+this.classes.popbox).is(":visible");
-    },
-
-    show: function(auto_close_ms, is_auto) { // is_auto = not manual call
-      var me = this;
-      if (me.is_visible()) return;
-
-      if (auto_close_ms) {
-        if (is_auto && me.auto_disabled) return;
-
-        $("."+this.classes.close_countdown_text).show(); // it's quite possible that popup will be displayed more than once, so if this countdown hidden, let's display it again.
-
-        me.disable_auto();
-        me.auto_shown = 1; // just a flag that we already displayed it.
-
-        // countdowns and misc stuff
-        $("."+me.classes.close_countdown).show();
-    
-        var auto_close_ticks = auto_close_ms / 1000;
-        $("."+me.classes.close_countdown_digits).html(auto_close_ticks);
-
-        me.auto_close_timer = setInterval(function() {
-
-          --auto_close_ticks;
-          $("."+me.classes.close_countdown_digits).html(auto_close_ticks);
-
-          if (auto_close_ticks == 0) me.hide();
-
-        }, 1000);
-      }else
-        $("."+me.classes.close_countdown).hide();
-
-      me.override_scrollbar(1);
-      $("."+me.classes.popbox).show();
-
-      if (this.on_show) this.on_show();
-    },
-
-    hide: function() {
-      this.stop_hide_timer();
-      $("."+this.classes.popbox).hide();
-      this.override_scrollbar(0);
-
-      if (this.on_hide) this.on_hide();
-    },
-
-    stop_hide_timer: function() {
-      if (this.auto_close_timer) {
-        clearInterval(this.auto_close_timer);
-        this.auto_close_timer = false;
-
-        $("."+this.classes.close_countdown_text).hide();
-      }
-    },
-
-    override_scrollbar: function(add) {
-      if (add) {
-        this.scrollY = $(window).scrollTop(); // (document.documentElement && document.documentElement.scrollTop) || document.body.scrollTop;
-        this.topProperties = window.getComputedStyle(document.body).top; // save properties
-        document.body.style.top = -this.scrollY + "px";
-        $("body").addClass(this.classes.fixed + " " + this.classes.no_overflow);
-
-      }else {
-        $("body").removeClass(this.classes.fixed + " " + this.classes.no_overflow);
-        document.body.style.top = this.topProperties; // restore properties
-        window.scrollTo(0, this.scrollY);
-      }
-    },
-  }
+  };
 
   if (!window.PopBox)
     window.PopBox = PopBox;
@@ -219,11 +252,20 @@
 /* USAGE example:
 
 PopBox.init({
-  auto_show: 15000,           // in milliseconds. 15000 milliseconds = 15 seconds. 0 = disabled.
-  auto_close: 60000,          // in milliseconds. 60000 = 60 seconds. 0 = disabled.
-  show_on_scroll_start: 45,   // starting scroll position in percents, between 0% and 100%. Both 0 = disabled.
-  show_on_scroll_end: 55,     // ending scroll position. Eg 40..60 means that popbox will appear when any part of page between 40% and 60% is appeared in the viewport.
-  closeable_on_dimmer: false,
-  auto_start_disabled: false, // disable auto_show on start. It can be re-enabled by calling PopBox.enable_auto() method. Set to "scroll" to start auto_show after any scroll event. (No scroll = no popup.)
+  autoShow: 15000,           // in milliseconds. 15000 milliseconds = 15 seconds. 0 = disabled.
+  autoClose: 60000,          // in milliseconds. 60000 = 60 seconds. 0 = disabled.
+  autoShowDisabled: false, // disable autoShow on start. It can be re-enabled by calling PopBox.startAutoShow() method. Set to "scroll" to start autoShow after any scroll event. (No scroll = no popup.)
+  showOnScrollStart: 45,   // starting scroll position in percents, between 0% and 100%. Both 0 = disabled.
+  showOnScrollEnd: 55,     // ending scroll position. Eg 40..60 means that popbox will appear when any part of page between 40% and 60% is appeared in the viewport.
+  closeOnDimmer: false,
+  closeOnEsc: true,
+  noPropagateClicks: false,
+  onShow: function() {     // callbacks
+     head_ext_resource("alertify/alertify-pbc.min.css", "link");
+     head_ext_resource("alertify/alertify.min.js", "script");
+  },
+  onHide: function() {
+     console.log('hidden');
+  },
 });
 */
